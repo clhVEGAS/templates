f536113c6b2b2ac6a0d9f34442b57715eadd1ae7
clhVEGAS <58777995+clhVEGAS@users.noreply.github.com>
2022-11-15 21:04:16 -0800

    FE + BE Created
    Frontend and Backend are created and communicating with each other in a docker-compose network.

---
+++ README.md
@@ -0,0 +1,5 @@
+Repo of frequently used templates.
+
+### DJANGO-REACT ###
+Forked from https://github.com/robdox/django-react-template
+Is a dockerized django, react, postgres application displaying a simple hello world function.
---
+++ django-react/api/api_app/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from datetime import datetime
+
+def index(request):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_date = datetime.now().strftime("%m/%d/%Y")
+
+    data = {
+        'time': current_time,
+        'date': current_date,
+    }
+
+    return JsonResponse(data)
